0517f8a90176aff5a95d319f5cddf2813a943477
DimiGreens <10632176@ucn.dk>
2026-01-05 09:46:12 +0100

    Tilføjet baseURL i Config-filen
    Tilføjet baseURL i config-filen (HUSK AT ÆNDRE LØBENDE)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  nitro: {
+    preset: 'static'
+  },
+  app: {
+    baseURL: '/class/MMD-CSD-S24/10632176/MMD3/Tester/'
+  },
   css: [
     '@/assets/css/style.css'
   ],
